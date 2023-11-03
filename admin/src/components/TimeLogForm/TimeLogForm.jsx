0ac7231cbd713731f5f928d1c97a273120673b8b
Chris Hyer <hyer.chris@gmail.com>
2023-11-02 18:51:23 -0700

    converted the unix time format into a human readable format when editing, then converted back to unix on server submit

--- admin/src/components/TimeLogForm/TimeLogForm.jsx
+++ admin/src/components/TimeLogForm/TimeLogForm.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import './TimeLogForm.css';
 
+/**
+ * Converts a date object to an input date string (YYYY-MM-DD).
+ *
+ * @param {Date} date - The date object to convert.
+ * @returns {string} The input date string.
+ */
 function convertToInputDate(date) {
     const dateObj = new Date(date);
     const year = dateObj.getFullYear();
@@ -9,6 +15,11 @@ function convertToInputDate(date) {
     return `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
 }
 
+/**
+ * Converts a date object to a string in the format of 'hh:mm:ss'.
+ * @param {Date} date - The date object to convert.
+ * @returns {string} The formatted time string.
+ */
 function convertToInputTime(date) {
     const dateObj = new Date(date);
     const hours = dateObj.getHours();
@@ -17,115 +28,157 @@ function convertToInputTime(date) {
     return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
 }
 
+/**
+ * Converts seconds to hours, minutes, and seconds format.
+ * @param {number} seconds - The total number of seconds to be converted.
+ * @returns {string} The formatted time string in the format of "hh:mm:ss".
+ */
 function secondsToHoursMinutesSeconds(seconds) {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds - (hours * 3600)) / 60);
-    const secondsLeft = seconds - (hours * 3600) - (minutes * 60);
-    return `${hours}:${minutes}:${secondsLeft}`;
+    const secondsLeft = Math.round(seconds - (hours * 3600) - (minutes * 60));
+    return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${secondsLeft < 10 ? '0' + secondsLeft : secondsLeft}`;
 }
 
+/**
+ * Converts hours, minutes, and seconds to total seconds.
+ *
+ * @param {number} hours - The number of hours.
+ * @param {number} minutes - The number of minutes.
+ * @param {number} seconds - The number of seconds.
+ * @returns {number} The total number of seconds.
+ */
 function hoursMinutesSecondsToSeconds(hours, minutes, seconds) {
-    return (hours * 3600) + (minutes * 60) + seconds;
+    return (Number(hours) * 3600) + (Number(minutes) * 60) + Number(seconds);
 }
 
+/**
+ * Parses a string in the format "hours:minutes:seconds" into an object with separate properties for hours, minutes, and seconds.
+ * @param {string} hoursMinutesSeconds - The string to parse.
+ * @returns {{hours: string, minutes: string, seconds: string}} - An object with separate properties for hours, minutes, and seconds.
+ */
 function parseHoursMinutesSeconds(hoursMinutesSeconds) {
     const [hours, minutes, seconds] = hoursMinutesSeconds.split(':');
     return { hours, minutes, seconds };
 }
 
+/**
+ * Creates a new Date object from a date and time string.
+ * @param {string} date - The date string in the format "YYYY-MM-DD".
+ * @param {string} time - The time string in the format "HH:MM:SS".
+ * @returns {Date} A new Date object representing the given date and time.
+ */
 function createDateTime(date, time) {
     const [year, month, day] = date.split('-');
-    const [hours, minutes, seconds] = time.split(':');
+    const [hours, minutes, seconds] = time.split(':').length === 2 ? [...time.split(':'), '00'] : time.split(':');
     return new Date(year, month - 1, day, hours, minutes, seconds);
 }
 
+/**
+ * Calculates the total time between a login and logout time.
+ * @param {Object} loginTime - The login time object containing date and time properties.
+ * @param {Object} logoutTime - The logout time object containing date and time properties.
+ * @returns {number} - The total time in seconds.
+ */
 function getTotalTime(loginTime, logoutTime) { 
     const loginDateTime = createDateTime(loginTime.date, loginTime.time);
     const logoutDateTime = createDateTime(logoutTime.date, logoutTime.time);
     const totalTime = logoutDateTime - loginDateTime;
-    return totalTime;
+    // console.log(totalTime);
+    return totalTime/1000;
 }
 
+/**
+ * A form component for logging time stamps of a student.
+ * @param {Object} props - The component props.
+ * @param {Object} props.student - The student object.
+ * @param {Function} props.onSave - The function to save the time log.
+ * @param {Function} props.onCancel - The function to cancel the time log.
+ * @returns {JSX.Element} - The TimeLogForm component.
+ */
 function TimeLogForm({ student, onSave, onCancel }) {
     const [timeStamps, setTimeStamps] = useState(student.loginTimestamps);
-    // const [timeIn, setTimeIn] = useState(student.timeIn);
-    // const [timeOut, setTimeOut] = useState(student.timeOut);
-    // const [hours, setHours] = useState(student.hours);
+    // console.log(timeStamps);
 
-    // const handleDateChange = (index, value) => {
-    //     const newDate = [...date];
-    //     newDate[index] = value;
-    //     setDate(newDate);
-    // };
-
-    // const handleTimeInChange = (index, value) => {
-    //     const newTimeIn = [...timeIn];
-    //     newTimeIn[index] = value;
-    //     setTimeIn(newTimeIn);
-    // };
-
-    // const handleTimeOutChange = (index, value) => {
-    //     const newTimeOut = [...timeOut];
-    //     newTimeOut[index] = value;
-    //     setTimeOut(newTimeOut);
-    // };
-
-    // const handleHoursChange = (index, value) => {
-    //     const newHours = [...hours];
-    //     newHours[index] = value;
-    //     setHours(newHours);
-    // };
-
-
-
-    // const handleRemoveTimeLog = (index) => {
-    //     const newDate = [...date];
-    //     const newTimeIn = [...timeIn];
-    //     const newTimeOut = [...timeOut];
-    //     const newHours = [...hours];
-    //     newDate.splice(index, 1);
-    //     newTimeIn.splice(index, 1);
-    //     newTimeOut.splice(index, 1);
-    //     newHours.splice(index, 1);
-    //     setDate(newDate);
-    //     setTimeIn(newTimeIn);
-    //     setTimeOut(newTimeOut);
-    //     setHours(newHours);
-    // };
-
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     const updatedStudent = { ...student, date, timeIn, timeOut, hours };
-    //     onSave(updatedStudent);
-    // };
-
+    /**
+     * Removes a time log from the list of time stamps.
+     * @param {number} index - The index of the time log to remove.
+     */
     const handleRemoveTimeLog = (index) => {
         const newTimeStamps = [...timeStamps];
         newTimeStamps.splice(index, 1);
         setTimeStamps(newTimeStamps);
-        console.log(newTimeStamps);
+        // console.log(newTimeStamps);
     };
 
+    /**
+     * Handles the change event of the class select input.
+     * @param {Object} event - The event object.
+     */
     const handleClassChange = (event) => {
         const updatedClass = event.target.value;
-        
+        // rest of the function code
     };
 
+    /**
+     * Handles changes to the time stamp inputs and calculates the total time.
+     * @param {Event} event - The event object.
+     */
+    const handleTimeStampChange = (event) => {
+        const parentDiv = event.target.parentNode.parentNode;
+        // const id = parentDiv.getAttribute('data-id');
+        // console.log(event.target.value);
+        // console.log(event.target.className);
+        let loginObj = {
+            date: event.target.className === 'login-date-value' ? event.target.value : String(parentDiv.querySelector('label input.login-date-value').value),
+            time: event.target.className === 'login-time-value' ? event.target.value : String(parentDiv.querySelector('label input.login-time-value').value),
+        };
+        let logoutObj = {
+            date: event.target.className === 'logout-date-value' ? event.target.value : String(parentDiv.querySelector('label input.logout-date-value').value),
+            time: event.target.className === 'logout-time-value' ? event.target.value : String(parentDiv.querySelector('label input.logout-time-value').value)
+        };
+
+        const totalTime = getTotalTime(loginObj, logoutObj);
+        const formattedTotalTime = secondsToHoursMinutesSeconds(totalTime);
+        parentDiv.querySelector('label input.total-time-value').value = formattedTotalTime;
+    };
+
+    /**
+     * Handles the form submission for the TimeLogForm component.
+     * @param {Event} event - The form submission event.
+     */
     const handleSubmit = (event) => {
         event.preventDefault();
         const docs = document.querySelectorAll('div[data-id]');
         let updatedTimeStamps = [];
         docs.forEach((doc) => {
-            const id = doc.getAttribute('data-id');
+            // const id = doc.getAttribute('data-id');
             const className = doc.querySelector('select.class-value').value;
-            const loginTime = Number(doc.querySelector('input.login-time-value').value);
-            const logoutTime = Number(doc.querySelector('input.logout-time-value').value);
-            const totalTime = Number(doc.querySelector('input.total-time-value').value);
-            updatedTimeStamps.push({ className, loginTime, logoutTime, totalTime });
+            const loginTime = doc.querySelector('input.login-time-value').value;
+            const loginDate = doc.querySelector('input.login-date-value').value;
+            const logoutTime = doc.querySelector('input.logout-time-value').value;
+            const logoutDate = doc.querySelector('input.logout-date-value').value;
+            const totalTime = doc.querySelector('input.total-time-value').value;
+
+            const { hours, minutes, seconds } = parseHoursMinutesSeconds(totalTime);
+            const newTotalTime = hoursMinutesSecondsToSeconds(hours, minutes, seconds);
+            const loginUnixTime = createDateTime(loginDate, loginTime).getTime();
+            const logoutUnixTime = createDateTime(logoutDate, logoutTime).getTime();
+
+            const updatedTimeLog = {
+                className,
+                loginTime: loginUnixTime,
+                logoutTime: logoutUnixTime,
+                totalTime: newTotalTime
+            };
+
+            // console.log(updatedTimeLog);
+            updatedTimeStamps.push(updatedTimeLog);
+
         });
         setTimeStamps(updatedTimeStamps);
         const updatedStudent = { ...student, loginTimestamps: updatedTimeStamps };
-        console.log(JSON.stringify(updatedStudent));
+        // console.log(JSON.stringify(updatedStudent));
         onSave(updatedStudent);
     };
 
@@ -150,17 +203,17 @@ function TimeLogForm({ student, onSave, onCancel }) {
                         </label>
                         <label>
                             Login:
-                            <input type='date' className="login-date-value" defaultValue={convertToInputDate(timeLog.loginTime)} />
-                            <input type='time' className="login-time-value" defaultValue={convertToInputTime(timeLog.loginTime)} />
+                            <input type='date' className="login-date-value" defaultValue={convertToInputDate(timeLog.loginTime)} onChange={handleTimeStampChange} />
+                            <input type='time' className="login-time-value" defaultValue={convertToInputTime(timeLog.loginTime)} onChange={handleTimeStampChange} />
                         </label>
                         <label>
                             Logout:
-                            <input type='date' className="logout-date-value" defaultValue={convertToInputDate(timeLog.logoutTime)} />
-                            <input type='time' className="logout-date-value" defaultValue={convertToInputTime(timeLog.logoutTime)} />
+                            <input type='date' className="logout-date-value" defaultValue={convertToInputDate(timeLog.logoutTime)} onChange={handleTimeStampChange} />
+                            <input type='time' className="logout-time-value" defaultValue={convertToInputTime(timeLog.logoutTime)} onChange={handleTimeStampChange} />
                         </label>
                         <label>
                             Total Time:
-                            <input type="text" readOnly className="total-time-value" defaultValue={timeLog.totalTime} />
+                            <input type="text" readOnly className="total-time-value" defaultValue={secondsToHoursMinutesSeconds(timeLog.totalTime)} />
                         </label>
                         <button type="button" onClick={() => handleRemoveTimeLog(index)}>Delete</button>
                     </div>
@@ -169,15 +222,6 @@ function TimeLogForm({ student, onSave, onCancel }) {
                 <button type="submit">Save</button>
                 <button type="button" onClick={onCancel}>Cancel</button>    
             </form>
-            {/* {student.loginTimestamps.map((timeLog, index) => (
-                <div key={index} className='timelog-row'>
-                    <p>Class: {timeLog.className}</p>
-                    <p>Login: {timeLog.loginTime}</p>
-                    <p>Logout: {timeLog.logoutTime}</p>
-                    <p>Total Time: {timeLog.totalTime} seconds</p>
-                </div>)
-            )}
-            <button type="button" onClick={onCancel}>Cancel</button> */}
         </div>
     );
 }

fcfe442e2afac32ca3db1ba24a24f68ba8d375ff
Chris Hyer <hyer.chris@gmail.com>
2024-05-21 14:22:29 -0700

    removed old admin page builds, added vercel build command, and vercel json file

--- admin/src/constants.js
+++
@@ -1,4 +0,0 @@
-const PORT = 5100;
-const prodURL = 'https://vivacious-jade-nightgown.cyclic.app';
-const devURL = `http://localhost:${PORT}`;
-export const URL = import.meta.env.MODE == 'development' ? devURL : prodURL;
